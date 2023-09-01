8d2a8e2eb699b80d56a9900a32103157cef907d3
victornwakpa <nwakpavictor@gmail.com>
2023-09-01 16:27:17 +0100

    Add class for active tab

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,11 +4,16 @@
 
 <template>
     <div>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink active-class="active" to="/">Home</RouterLink>
+        <RouterLink active-class="active" to="/about">About</RouterLink>
     </div>
 </template>
 
 <style scoped>
-    
+    .active {
+        color: blueviolet;
+        font-weight: 700;
+        margin: 0 10px;
+        text-decoration: none;
+    }
 </style>
